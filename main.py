6ed7522cfcaa97dbfc2adee510de2667026f8d05
suphakon.pr <suphakon.pra@ofm.co.th>
2025-04-09 15:59:58 +0700

    router

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-from typing import Union
+from fastapi import APIRouter, FastAPI
+import uvicorn
+from router import webhook
 
-from fastapi import FastAPI
+def get_router() -> APIRouter:
+    
+    router = APIRouter()
+    router.include_router(webhook.router, prefix="/webhook")
+    
+    return router
 
-app = FastAPI()
+def get_app() -> FastAPI:
+    
+    app = FastAPI()
+    app.include_router(get_router())
+
+    return app
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+
+app = get_app()
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8000)
